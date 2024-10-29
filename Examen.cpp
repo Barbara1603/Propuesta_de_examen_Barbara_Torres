58d0fe60aa73a7c42cf17b3bfb5b7320762525df
Bárbara Torres Sulca <btorrsul@myuax.com>
2024-10-29 11:57:11 +0100

    examen

--- Examen.cpp
+++ Examen.cpp
@@ -3,33 +3,27 @@
 //
 
 #include "Examen.h"
-#include <stdio.h>
+#include <iostream>
 
 
-int esTriplePitagorico(int a, int b, int c) {
-    return (a * a + b * b == c * c);
+bool esTriplePitagorico(int a, int b, int c) {
+    return (a * a + b * b == c * c) &&
+           (a + b > c) && (a + c > b) && (b + c > a);
 }
 
 int main() {
-    int triples[4][3] = {
-        {3, 4, 5},
-        {6, 8, 10},
-        {5, 12, 13},
-        {8, 15, 17}
-    };
+    int triples[4][3] = {{3, 4, 5}, {6, 8, 10}, {5, 12, 13}, {8, 15, 17}};
 
-    printf("Triples pitagoricos especificados:\n");
-
-    for (int i = 0; i < 4; i++) {
+    std::cout << "Triples pitagóricos encontrados:\n";
+    for (int i = 0; i < 4; ++i) {
         int a = triples[i][0];
         int b = triples[i][1];
         int c = triples[i][2];
 
         if (esTriplePitagorico(a, b, c)) {
-            printf("%d - %d - %d\n", a, b, c);
+            std::cout << a << " - " << b << " - " << c << std::endl;
         }
     }
 
     return 0;
 }
-
